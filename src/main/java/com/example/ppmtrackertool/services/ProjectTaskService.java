b2645c9673432734da2a52cb43d453ab21ba73a3
Piyush Chopra <piyush.chopra@payu.in>
2019-09-13 23:37:53 +0530

    git branch 34+35 with bug fixed & projectservice.java & backlogcontroller.java added to git

---
+++ src/main/java/com/example/ppmtrackertool/services/ProjectTaskService.java
@@ -0,0 +1,49 @@
+package com.example.ppmtrackertool.services;
+
+import com.example.ppmtrackertool.domain.Backlog;
+import com.example.ppmtrackertool.domain.ProjectTask;
+import com.example.ppmtrackertool.repository.BacklogRepository;
+import com.example.ppmtrackertool.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        //Exceptions: Project not found
+
+        //PTs to be added to a specific project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //set the bl to pt
+        projectTask.setBacklog(backlog);
+        //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+        // Update the BL SEQUENCE
+        BacklogSequence++;
+
+        backlog.setPTSequence(BacklogSequence);
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+        projectTask.setProjectIdentifer(projectIdentifier);
+
+        //INITIAL priority when priority null
+//        if(projectTask.getPriority()==0||projectTask.getPriority()==null){
+//            projectTask.setPriority(3);
+//        }
+        //INITIAL status when status is null
+        if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+
+}
